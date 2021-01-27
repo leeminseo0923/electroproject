f5311427a3336c51a84a3a7661558cda1db19012
leeminseo0923 <leeminseo0923@gmail.com>
2021-01-27 22:47:44 +0900

    spinbox

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QHBoxLayout, QVBoxLayout, QComboBox, QLabel, QDoubleSpinBox, QSpinBox
 
 class MyApp(QWidget):
     def __init__(self):
@@ -8,7 +8,57 @@ class MyApp(QWidget):
 
     def initUI(self):
         self.setWindowTitle('TEST PROGRAM')
+
+        self.cal_button = QPushButton('Calculate')
+        self.data_cell = QLabel('a')
+        self.input_num = QSpinBox()
+        self.input_num.setValue(1)
+        self.input_num.valueChanged.connect(self.changeCellNum)
+        self.input_cell_x = [QDoubleSpinBox()]
+        self.input_cell_y = [QDoubleSpinBox()]
+        self.hbox = QHBoxLayout()
+        self.vbox = QVBoxLayout()
+        self.inputbox_x = QHBoxLayout()
+        self.inputbox_y = QHBoxLayout()
+        self.inputbox_xy = QVBoxLayout()
+        self.inputbox = QHBoxLayout()
+
+        self.vbox.addWidget(self.data_cell)
+        self.inputbox.addWidget(self.input_num)
+        self.inputbox_x.addWidget(self.input_cell_x[0])
+        self.inputbox_y.addWidget(self.input_cell_y[0])
+        self.inputbox_xy.addLayout(self.inputbox_x)
+        self.inputbox_xy.addLayout(self.inputbox_y)
+        self.inputbox.addLayout(self.inputbox_xy)
+        self.vbox.addLayout(self.inputbox)
+        self.vbox.addWidget(self.cal_button)
+        self.hbox.addLayout(self.vbox)
+
+        self.setLayout(self.hbox)
+
+
         self.showMaximized()
+
+    def changeCellNum(self, num):
+        current = len(self.input_cell_x)
+        if num > current:
+            for i in range(num-current):
+                self.input_cell_x.append(QDoubleSpinBox())
+                self.inputbox_x.addWidget(self.input_cell_x[-1])
+                self.input_cell_y.append(QDoubleSpinBox())
+                self.inputbox_y.addWidget(self.input_cell_y[-1])
+        elif num < current:
+            for i in range(current-num):
+                self.inputbox_x.removeWidget(self.input_cell_x[-1])
+                del self.input_cell_x[-1]
+                self.inputbox_y.removeWidget(self.input_cell_y[-1])
+                del self.input_cell_y[-1]
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
